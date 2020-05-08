2bc0247ee2bc844d266f3e78502e8f4273b1e853
Amir Habib <amir@acomp.co.il>
2020-05-08 11:54:36 +0300

    constructor

--- index.php
+++ index.php
@@ -6,17 +6,27 @@ class Person {
             $age, 
             $email;
 
+    function __construct($name = 'nobody')
+    {
+        //  Initiate default values
+        $this->name = $name;
+
+        $this->age = 18;
+        $this->email = '';
+    }
+
     function sayHi() {
 
         return "Hi my name is $this->name";
     }
 }
 
-$p1  = new Person;
+$p1  = new Person('John');
 $p2 =  new Person;
+$p3 =  new Person;
 
-$p1->name = 'John';
 $p2->name = 'Jane';
 
-echo $p1->sayHi() . "<br>";
-echo $p2->sayHi() . "<br>";
+echo $p1->sayHi() . "<br>";     //  John    
+echo $p2->sayHi() . "<br>";     //  Jane
+echo $p3->sayHi() . "<br>";     //  nobody
