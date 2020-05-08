531313f7d2a95fa1a0d6ed66c18f83bc43fe4548
Amir Habib <amir@acomp.co.il>
2020-05-08 11:16:43 +0300

    functions part 3 - str_replace

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
 $greeting = "Hello world how are you today?";
 
-$words = explode(" ", $greeting);
+$new_greeting = str_replace("world", "there", $greeting);
 
-print_r( $words );
-
-$greeting_with_underscore = implode("_", $words);
-
-echo $greeting_with_underscore;
+echo $new_greeting;
