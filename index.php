36847fd607f16c774809afed9b11ba2e5036b1d6
Amir Habib <amir@acomp.co.il>
2020-05-08 11:02:40 +0300

    Working with JSON

--- index.php
+++ index.php
@@ -1,42 +1,52 @@
 <?php
 
-    $library = [];
 
-    $library['php'] = [
-        "php and Mysql by O'reilly",
-        "PHP 7 by Wrox",
-        "Professional PHP by Wrox"
+    /*
+
+    JSON
+
+    person = {
+        name: 'Amir',
+        email: 'amir@acomp.co.il',
+        hobbies: [
+            'keyboard',
+            'books'
+        ],
+        address: {
+            street: 'bla',
+            city: 'Haifa'       
+        }
+    }
+
+    */
+
+
+    $person = [
+        "name" => "Amir",
+        "email" => "amir@acomp.co.il",
+        "hobbies" => [
+            'keyboard', 
+            'books'
+        ],
+        "address" => [
+            "street" => "bla",
+            "city" => "Haifa"
+        ]
     ];
 
+    //  Convert $person to JSON string
+    //  echo json_encode($person);
 
-    $library['css'] = [
-        'CSS3 without a framework',
-        'Bootstrap 5',
-        'CSS Cookbook'
-    ];
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Books</h1>
+    //  --------------------------------
 
-    <?php foreach($library as $category => $books) : ?>
-    <div>
-        <h2><?= $category; ?></h2>
-        <ul>
-            <?php foreach($books as $bookName) : ?>
-            <li><?= $bookName ?></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <?php endforeach; ?>
+    //  Convert JSON to an assoc array
 
-</body>
-</html>
+    $inputFromAngular = '{"name":"Amir","email":"amir@acomp.co.il","hobbies":["keyboard","books"],"address":{"street":"bla","city":"Haifa"}}';
+
+
+    $input = json_decode($inputFromAngular, true);
+
+    //echo "Hello " . $input['name'];
+
+    print_r( $input );
