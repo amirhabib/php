ce91d95d5320ca9aaec9e9d1b254ca23c0cd37cb
Amir Habib <amir@acomp.co.il>
2020-05-08 09:37:31 +0300

    Using if statements

--- index.php
+++ index.php
@@ -1,13 +1,51 @@
 <?php
 
-    $name = "Amir";                                                     //  String in double quotation marks
-    $email = 'amir@blabla.com';                                         //  String is single quotation marks
+        $covide_notice_active = false;
 
-    echo "Hello $name <br>";                                            //  Hello Amir
-    echo 'Hello $name <br>';                                            //  Hello $name (because string is in single qoutes)
-
-    echo "Hello $name your email is $email<br>";                        //  Hello Amir your email is amir@blabla.com
-
-    //  String concatination
-    echo "Hello" . $name . " your email is " . $email . "<br>";         //  Hello Amir your email is amir@blabla
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello there</h1>
+
+    <?php
+
+
+        //  OPTION 1
+
+        // if($covide_notice_active) {
+        //     echo "<div>";
+        //     echo "Due to COVID-19 please wash your hands.";
+        //     echo "</div>";
+        // }
+
+    ?>
+
+
+    <?php
+
+        //  OPTION 2 
+
+        if($covide_notice_active) {
+
+    ?>
+        <div>
+            Due to COVID-19 please wash your hands.
+        </div>
+    <?php } ?>
+
+
+
+    <?php if($covide_notice_active) : ?>
+        <div>
+            Due to COVID-19 please wash your hands.
+        </div>
+    <?php endif ?>
+
+</body>
+</html>
