11d6ea8f7877167ef5b0f58f14f5a8e423181224
Amir Habib <amir@acomp.co.il>
2020-05-08 09:25:33 +0300

    String concatination

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo "Hello World " . date( "Y/M/d H:i") ?></h1>
-</body>
-</html>
+<?php
+
+    $name = "Amir";                                                     //  String in double quotation marks
+    $email = 'amir@blabla.com';                                         //  String is single quotation marks
+
+    echo "Hello $name <br>";                                            //  Hello Amir
+    echo 'Hello $name <br>';                                            //  Hello $name (because string is in single qoutes)
+
+    echo "Hello $name your email is $email<br>";                        //  Hello Amir your email is amir@blabla.com
+
+    //  String concatination
+    echo "Hello" . $name . " your email is " . $email . "<br>";         //  Hello Amir your email is amir@blabla
+?>
